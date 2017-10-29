02a623c1293968a6122fdf0fe3496fee40b46f40
erickolbusz <erickolbusz@gmail.com>
2017-10-29 01:35:30 -0400

    usage logs

--- work.c
+++ work.c
@@ -16,7 +16,7 @@ void timeToStr(char * buf, time_t seconds) {
     seconds -= (hours*3600);
     int minutes = seconds/60;
     seconds -= (minutes*60);
-    sprintf(buf, "TOTAL TIME: %dd %dh %dm %ds\0", days, hours, minutes, seconds);
+    sprintf(buf, "%dd %dh %dm %ds\0", days, hours, minutes, seconds);
 }
 
 time_t strToTime(char * buf) {
@@ -37,25 +37,42 @@ void write_time(int signal) {
     time_t end = time(0);
     time_t diff = end - start;
 
-    char in_buf[100];
+    char bufIn[100];
     int f = open("time.txt", O_RDWR | O_CREAT, S_IRWXU | S_IRWXG);
-    read(f, in_buf, 100);
+    read(f, bufIn, 100);
 
-    time_t old_time = strToTime(in_buf);
+    time_t old_time = strToTime(bufIn);
     time_t new_time = old_time + diff;
 
-    char out_buf[107];
-    timeToStr(out_buf, new_time);
-
+    char bufTmp1[100];
+    char bufTotal[107];
+    timeToStr(bufTmp1, new_time);
+    sprintf(bufTotal, "TOTAL TIME: %s", bufTmp1);
     int pad = 0;
-    while (out_buf[pad]) {pad++;}
-    while (pad < 100) {out_buf[pad++] = ' ';}
-    while (pad < sizeof(out_buf)) {out_buf[pad++] = '-';}
-    out_buf[100] = '\r';
-    out_buf[101] = '\n';
+    while (bufTotal[pad]) {pad++;}
+    while (pad < 100) {bufTotal[pad++] = ' ';}
+    while (pad < sizeof(bufTotal)) {bufTotal[pad++] = '-';}
+    bufTotal[100] = '\r';
+    bufTotal[101] = '\n';
+
+    char bufInfo[100];
+    char bufTmp2[100];
+    timeToStr(bufTmp2, diff);
+    struct tm startTime = *localtime(&start);
+    sprintf(bufInfo, "\r\n%d-%d-%d %d:%d:%d\t%s", startTime.tm_year+1900, 
+            startTime.tm_mon+1, startTime.tm_mday, 
+            startTime.tm_hour, startTime.tm_min,
+            startTime.tm_sec, bufTmp2);
 
     lseek(f,0,SEEK_SET);
-    write(f, out_buf, sizeof(out_buf));
+    write(f, bufTotal, sizeof(bufTotal));
+
+    int write_len = 0;
+    while (bufInfo[++write_len]){}
+
+    lseek(f,0,SEEK_END);
+    write(f, bufInfo, write_len);
+
     close(f);
     exit(0);
 }
